7a82c74a35641011cc82fd83dd373996cb364ab9
dawei <dawei@i>
2015-03-06 22:19:04 -0800

    Initial commit.

--- sci calc/ViewController.swift
+++ sci calc/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    
+    var brain = CalculatorBrain()
+
+    @IBAction func appendDigit(sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            // assignment 2 puts error message here
+            displayValue = 0
+        }
     }
 
-
+    var displayValue: Double {
+        get {
+            if display.text! == "π" {
+                return M_PI
+            } else {
+                return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+            }
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    @IBAction func allClear(sender: UIButton) {
+        display.text = "0"
+        userIsInTheMiddleOfTypingANumber = false
+        brain.allClear()
+    }
+    
 }
 
